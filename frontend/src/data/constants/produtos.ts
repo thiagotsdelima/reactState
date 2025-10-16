1e05c08c997125b1a541eec1694c2e3084ba2ed7
thiago <thiagotsdelima@outlook.com>
2025-10-16 07:13:28 -0300

    created data fictitious and interface

---
+++ frontend/src/data/constants/produtos.ts
@@ -0,0 +1,62 @@
+import Produtos from '../model/Produtos'
+
+const produtos: Produtos[] = [
+  {
+    id: 1,
+    nome: 'Camiseta',
+    preco: 39.9,
+    descricao: 'Camiseta branca de algodão',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/teste-formacao-dev.appspot.com/o/curso-unsplash%2FUntitled.jpg?alt=media&token=7ca012dd-b047-4103-bb03-1e1d84b26d27'
+  },
+  {
+    id: 2,
+    nome: 'Calça Jeans',
+    preco: 99.9,
+    descricao: 'Calça azul de algodão',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Fcalca-jeans.jpg?alt=media&token=5ec4a365-7b08-4761-9e26-f1b5bd309d16'
+  },
+  {
+    id: 3,
+    nome: 'Tênis',
+    preco: 129.9,
+    descricao: 'Tênis esportivo',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Ftenis.jpg?alt=media&token=5da00575-a808-4c91-abdb-54ce9d25b31a'
+  },
+  {
+    id: 4,
+    nome: 'Camisa Polo',
+    preco: 49.9,
+    descricao: 'Camisa polo azul',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Fcamiseta-polo.jpg?alt=media&token=27f695e6-556a-4501-ba5d-a02b42fdc015'
+  },
+  {
+    id: 5,
+    nome: 'Camisa Social',
+    preco: 79.9,
+    descricao: 'Camisa social branca',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Fcamisa-social.jpg?alt=media&token=51d85b62-3500-4497-aa32-236d04e27356'
+  },
+  {
+    id: 6,
+    nome: 'Blusa',
+    preco: 29.9,
+    descricao: 'Blusa de lã',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Fblusa.jpg?alt=media&token=b4c64617-72c0-484b-a5de-e183e6b7083e'
+  },
+  {
+    id: 7,
+    nome: 'Jaqueta',
+    preco: 129.9,
+    descricao: 'Jaqueta de couro',
+    imagem:
+      'https://firebasestorage.googleapis.com/v0/b/formacao-dev.appspot.com/o/cursos-assets%2Fgerenciamento-de-estado%2Fjaqueta-couro.jpg?alt=media&token=916bfd2c-14a8-482f-91ed-92e9355931fc'
+  }
+]
+
+export default produtos
