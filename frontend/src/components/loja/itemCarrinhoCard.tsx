9befb910e81e04ff9125a87cd96752f88b93170c
thiago <thiagotsdelima@outlook.com>
2025-11-03 10:09:31 -0300

    feat(function):add item in cart

---
+++ frontend/src/components/loja/itemCarrinhoCard.tsx
@@ -0,0 +1,28 @@
+import { CarrinhoContext } from '@/context/CarrinhoContext'
+import { ItemCarrinho } from '@/data/model/ItemCarrinho'
+import { IconMinus, IconPlus } from '@tabler/icons-react'
+import { useContext } from 'react'
+
+interface ItemCarrinhoCardProps {
+  item: ItemCarrinho
+}
+
+export default function ItemCarrinhoCard(props: ItemCarrinhoCardProps) {
+  const { item } = props
+  const { adicionarItem } = useContext(CarrinhoContext)
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5 bg-black rounded-b-md p-2">
+      <span className="text-center">{item.produtos.nome}</span>
+      <div className="flex gap-2 items-center">
+        <button className="h-5 w-5 bg-red-500 rounded-b-sm flex justify-center items-center">
+          <IconMinus />
+        </button>
+        <span>{item.quantidade}</span>
+        <button className="h-5 w-5 bg-green-500 rounded-b-sm flex justify-center items-center">
+          <IconPlus onClick={() => adicionarItem(item.produtos)} />
+        </button>
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/loja/produtos.tsx
+++ frontend/src/components/loja/produtos.tsx
@@ -2,12 +2,19 @@ import { useContext } from 'react'
 import Area from '../template/Area'
 import CarrinhoVazio from './carrinhoVazio'
 import { CarrinhoContext } from '@/context/CarrinhoContext'
+import ItemCarrinhoCard from './itemCarrinhoCard'
 
 export default function Produtos() {
   const { itens } = useContext(CarrinhoContext)
   return (
     <Area titulo="Produtos" cor="black">
-      {itens.length === 0 ? <CarrinhoVazio /> : <div>tem Itens</div>}
+      {itens.length === 0 ? (
+        <CarrinhoVazio />
+      ) : (
+        itens.map((item) => {
+          return <ItemCarrinhoCard key={item.produtos.id} item={item} />
+        })
+      )}
     </Area>
   )
 }
