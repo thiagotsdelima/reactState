d15ba42df9580a2678b82ff72f66276efe7fd2cd
thiago <thiagotsdelima@outlook.com>
2026-01-14 08:01:08 -0300

    commit finished

--- frontend/src/components/ead/estatisticas.tsx
+++ frontend/src/components/ead/estatisticas.tsx
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
 import Estatistica from './estatistica'
+import CursoContext from '@/context/CursoContext'
 
 export default function Estatisticas() {
+  const {
+    qtdeDeAulas,
+    aulasConcluidas,
+    duracaoConcluida,
+    percentualConclusao,
+    duracaoTotal
+  } = useContext(CursoContext)
   return (
     <div className="flex justify-around items-center w-4/5 h-24 bg-white/50 rounded-lg">
-      <Estatistica valor={10} titulo="Qtde de aula" />
-      <Estatistica valor={5} titulo="Aulas concluidas" />
-      <Estatistica valor="2h:30" titulo="Duracao total" />
-      <Estatistica valor="1h:24" titulo="Duracao concluida" />
-      <Estatistica valor="58%" titulo="Perc. Concluido" />
+      <Estatistica valor={qtdeDeAulas} titulo="Qtde de aula" />
+      <Estatistica valor={aulasConcluidas} titulo="Aulas concluidas" />
+      <Estatistica valor={duracaoTotal} titulo="Duracao total" />
+      <Estatistica valor={duracaoConcluida} titulo="Duracao concluida" />
+      <Estatistica valor={`${percentualConclusao}%`} titulo="Perc. Concluido" />
     </div>
   )
 }
